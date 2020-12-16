373d6f046f7b5c14593f057831cf529f4e721fb9
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2020-12-16 15:28:44 +0100

    Add helloWorldConnector class and test files

---
+++ src/main/resources/archetype-resources/src/main/java/connector/HelloWorldConnector.java
@@ -0,0 +1,13 @@
+package ${package}.connector;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorldConnector
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
